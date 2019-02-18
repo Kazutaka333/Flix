9d96feef2fcddd3bf4a7a068a717f7f2f0a2b5a2
Kazutaka Homma <special18fender@gmail.com>
2019-02-18 00:18:56 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Kazutaka Homma on 2/18/19.
+//  Copyright © 2019 Kazutaka Homma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
