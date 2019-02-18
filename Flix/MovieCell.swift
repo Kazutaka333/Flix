189b22fb7461fcc1dcc72e8884df9103a4a6681a
Kazutaka Homma <special18fender@gmail.com>
2019-02-18 01:26:17 -0800

    finish all the user stories

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MoviewCell.swift
+//  Flix
+//
+//  Created by Kazutaka Homma on 2/18/19.
+//  Copyright © 2019 Kazutaka Homma. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [[String: Any]] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        downloadData()
+    }
+    
+    func downloadData() {
+        let url = URL(string:  "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                // TODO: Get the array of movies
+                let movies: [[String: Any]] = dataDictionary["results"] as! [[String: Any]]
+                
+                // TODO: Store the movies in a property to use elsewhere
+                self.movies = movies
+                
+                // TODO: Reload your table view data
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
+}
+
+
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let movie = movies[indexPath.row]
+        let imgPath = movie["poster_path"] as! String
+        let imgUrl = URL(string: baseUrl + imgPath)!
+        let title = movie["title"] as! String
+        let description = movie["overview"] as! String
+        cell.posterImageView.af_setImage(withURL: imgUrl)
+        cell.titleLabel.text = title
+        cell.descriptionLabel.text = description
+        return cell
+    }
 }
+//
+//extension ViewController : UITableViewDelegate {
+//    
+//}
 
